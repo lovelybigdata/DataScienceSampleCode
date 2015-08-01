8dc431a4ae3bc08bb85720287c8b7f6c54e4b94f
tim <tim.yellow@gmail.com>
2015-08-01 09:44:55 +0800

    add more example

---
+++ escape.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+print "Romio\tJuia"
+print r"True \t lie"
+
+# 比較一下　有沒有加上　r 在前面代表什麼意思？
+
---
+++ string_replace.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+article = "國父孫中山先生名叫孫中山先生"
+　　
+
+# 使用　atricle.replace 讓孫中山先生的前面孫空一格
+# 請　google  python string replace　或是 在　python shell 內打　help(article.replace)
+# PS: 離開　help 要按個　q
