360eb3bb8df85199c2ce1a54e117ee4957da8110
tim <tim.yellow@gmail.com>
2015-08-01 13:11:58 +0800

    add sort function

--- blog_page.py
+++ blog_page.py
@@ -2,6 +2,10 @@
 from bottle import route, run, template, view, debug
 
 
+def article_compare(article1, article2):
+    return article1['lat'] - article2['lat']
+
+
 @route('/blog/')
 @route('/')
 @view('blog_template')
@@ -10,25 +14,31 @@ def hello():
     post_list = [
         {
             'title': '台南雙日遊',
-            'lat': 'xxx',
-            'lng': 'oo',
+            'lat': 22,
+            'lng': 121,
             'content': '很多東西可以吃',
             'photo': 'http://ext.pimg.tw/mimg47/1391871607-3002859607.jpg',
             'id': 0,
         },
         {
             'title': '北海岸牛排',
+            'lat': 25,
+            'lng': 121,
             'content': '夫妻吵架，各開一家',
             'photo': 'http://ext.pimg.tw/mimg47/1391871607-3002859607.jpg',
             'id': 1,
         },
         {
-            'title': '',
+            'title': 'yo',
+            'lat': 24,
+            'lng': 121,
             'content': '夫妻吵架，各開一家',
             'photo': 'http://ext.pimg.tw/mimg47/1391871607-3002859607.jpg',
             'id': 1,
         },
     ]
+
+    post_list.sort(cmp=article_compare)
 
     return {
             'post_list': post_list
