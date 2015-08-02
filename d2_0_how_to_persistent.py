f5d78f94438bc7c024ea004781e383d2047a373f
tim <tim.yellow@gmail.com>
2015-08-02 10:44:57 +0800

    update

--- d2_0_how_to_persistent.py
+++ d2_0_how_to_persistent.py
@@ -2,4 +2,5 @@ from __future__ import print_function
 
 count = 0
 count += 1
+
 print(count)
--- d2_2_blog_sql.py
+++ d2_2_blog_sql.py
@@ -7,8 +7,10 @@ def blog_all():
     conn = sqlite3.connect('Blog.sqlite')
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    c.execute('select * from blog')
-    return c.fetchall()  # You get a list of dict like object
+    c.execute("SELECT * FROM  blog ORDER BY lng desc")
+    data = c.fetchall()
+    conn.close()
+    return data
 
 
 # http://localhost:8080/blog_sql/
